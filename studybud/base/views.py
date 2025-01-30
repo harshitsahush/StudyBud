b98ff6f38bef7e42141a8bb31ec8c578eea51d3a
Harshit Sahu <harshitsahu.sh@gmail.com>
2025-01-30 13:22:46 +0530

    Included basic templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
 def home(request):
-    return HttpResponse("Homepage")
+    return render(request, "home.html")
 
 def room(request):
-    return HttpResponse("This is a room page")
+    return render(request, "room.html")
