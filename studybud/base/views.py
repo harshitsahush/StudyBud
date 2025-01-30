bad8d5628d67f97d6bed14c3222fcec202e0f118
Harshit Sahu <harshitsahu.sh@gmail.com>
2025-01-30 17:41:49 +0530

    Sending data from backend to templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    rooms = [
+        {'id':1, "name":"Discuss about python"},
+        {'id':2, "name":"anything about UX"},
+        {'id':3, "name":"lessons on node.js"},
+    ]
+
+    context = {"rooms" : rooms}
+
+    return render(request, "home.html", context)
 
 def room(request):
     return render(request, "room.html")
