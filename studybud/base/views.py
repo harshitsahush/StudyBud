1e74538052c46ace8c0e4feab6919729d01b8008
Harshit Sahu <harshitsahu.sh@gmail.com>
2025-02-03 19:16:44 +0530

    Fetching rooms from DB

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
+""" rooms = [
     {'id':1, "name":"Discuss about python"},
     {'id':2, "name":"anything about UX"},
     {'id':3, "name":"lessons on node.js"},
-]
+] """
+
+
 
 def home(request):
+    #fetch all rooms from DB
+    rooms = Room.objects.all()
+
     context = {"rooms" : rooms}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if(str(i['id']) == pk):
-            room = i
+    #fetch the room with the id as pk
+    room = Room.objects.get(id = pk)                #get room info where id = pk
 
     context = {"room" : room}                       #send room to frontend
     return render(request, "base/room.html", context)
